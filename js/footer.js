47e0db9e497c08d8e5871a6a946f332b991861a7
needleandhaystackent <admin@needleandhaystackent.com>
2025-03-04 03:45:16 -0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,5 @@
-fetch("footer.html")
-    .then(response => response.text())
-    .then(data => document.body.insertAdjacentHTML("beforeend", data));
+document.write(`
+<footer class="footer">
+    <p>&copy; 2025 Needle and Haystack Entertainment | <a href="privacy.html">Privacy Policy</a></p>
+</footer>
+`);
